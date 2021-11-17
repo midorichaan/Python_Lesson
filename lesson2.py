be989166b7f4e06be4ba57266b1fdcb5cab81968
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-17 21:37:23 +0900

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,39 @@
+#文字列
+str()
+a = "aaa"
+
+#数値
+int() → 整数値
+float() → 浮動小数点
+
+a = 5
+a = 5.0
+
+#bool
+bool()
+True = はい
+→ 1
+False = いいえ
+→ 0
+
+#datetime
+datetime()
+
+#list
+list()
+[]
+
+#tuple
+tuple()
+()
+
+#dic
+dict()
+{}
+
+{"a": "value"}
+
+#数値から文字列へ
+n = 5 → 数値
+str(n) → 文字列
+n = "5"

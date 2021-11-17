266015340c3eb27a72c059315bf6792bbff425ff
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-17 21:18:56 +0900

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,9 +1,23 @@
 #文字列の出力#
-
 print("hello world")
 print()
 
 #ファイル/ライブラリの読み込み
-import discord
-#import ○○
+import library
 
+#コメントアウト
+#comment here
+"aaa" 'aaa'
+
+def test():
+    #print("hello world")
+    print("Hello World!")
+    
+"aaa" → ok
+"aaa → error
+'aa "aaa"'
+string
+
+"aaaa"
+
+str() → 文字列に変換

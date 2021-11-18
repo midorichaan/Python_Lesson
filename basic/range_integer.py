5e45dc0c73beb4817b3e2f20f7ac8517af9b3887
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-18 15:32:44 +0900

    Update range_integer.py

--- basic/range_integer.py
+++ basic/range_integer.py
@@ -4,3 +4,15 @@ def integer():
 
 if __name__ == "__main__":
     integer()
+
+
+"""
+コードの解説
+
+def integer(): #integerという名前の関数を作成 
+    for i in range(30): #range関数をforループでi代入し下記で処理
+        print(i) #数値 i を出力
+
+if __name__ == "__main__": #python3 名前.py で実行されているかチェック
+    integer() #関数 integer の実行
+"""

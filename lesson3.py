2e2a498e6a1b73f9fd71d85d76ba07ebf5035b03
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-18 15:17:04 +0900

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -1,8 +1,12 @@
 #実際に書いてみよう 基礎編1
-#答えはここから: https://github.com/midorichaan/Python_Lesson/basic/helloworld.py
 
 """
 1. ターミナルに Hello world! を出力するプログラムを書こう！
 
-ヒント: def, print()
+ヒント: def, print(), if, __name__, '__main__'
+
+答えはここから↓
+https://github.com/midorichaan/Python_Lesson/basic/helloworld.py
 """
+
+

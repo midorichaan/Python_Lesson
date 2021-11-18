62546e95d33f2ea7dfbe98f8563e71cb1e422684
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-18 14:05:20 +0900

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,23 +1,43 @@
-#文字列の出力#
+#文字列の出力
+"""
+文字列の出力を行うには 以下のようなprint関数を使用します。
+()内の文字等が標準出力されます。
+"""
 print("hello world")
-print()
+
 
 #ファイル/ライブラリの読み込み
+"""
+ファイル/ライブラリの読み込みはimport文を使用します。
+import 名前
+
+Ex. discord.pyを使いたい時は
+import discord
+"""
 import library
 
-#コメントアウト
-#comment here
-"aaa" 'aaa'
 
+#コメントアウト
+"""
+文字列をコメントアウトするには # を使用します。
+コメントアウトされた行はコメントとして扱われます。
+"""
+print("hello world") #コメント
+#コメント
+
+#関数
+"""
+関数とはあるデータを受け取り、定められた独自の処理を実行し、その結果を返す命令のことです。
+Pythonでの関数は async def ... / def ... のようになります。
+
+- 同期関数
+def 名前(引数):
+    #処理
+    
+- 非同期関数
+async def 名前(引数):
+    #処理
+"""
 def test():
     #print("hello world")
     print("Hello World!")
-    
-"aaa" → ok
-"aaa → error
-'aa "aaa"'
-string
-
-"aaaa"
-
-str() → 文字列に変換

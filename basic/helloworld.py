7a3b11228cc2a594dc9624c876ffb5b02462f4f6
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-18 15:07:45 +0900

    Update helloworld.py

--- basic/helloworld.py
+++ basic/helloworld.py
@@ -3,3 +3,13 @@ def hello():
 
 if __name__ == "__main__":
     hello()
+
+"""
+コードの解説
+
+def hello(): #ここで関数 hello の作成
+    print("Hello world!") #hello が実行された時に行う処理。 ここでは Hello world! を出力する
+
+if __name__ == "__main__": #ここで python3 helloworld.py が実行されているかチェック
+    hello() #上のチェックがTrueの時に関数 hello を実行する
+"""

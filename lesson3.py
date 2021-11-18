1ad42fd49805e37f188a4605d063d41fe5446b4d
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-18 15:05:06 +0900

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -1,3 +1,12 @@
-#実際に書いてみよう
-#https://github.com/midorichaan/Python_Lesson/basic/helloworld.py
+#実際に書いてみよう 基礎編1
+#答えはここから: https://github.com/midorichaan/Python_Lesson/basic/helloworld.py
 
+"""
+コードの解説
+
+def hello(): #ここで関数 hello の作成
+    print("Hello world!") #hello が実行された時に行う処理。 ここでは Hello world! を出力する
+
+if __name__ == "__main__": #ここで python3 helloworld.py が実行されているかチェック
+    hello() #上のチェックがTrueの時に関数 hello を実行する
+"""
